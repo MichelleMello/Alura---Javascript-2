e4d096f4fffea6f4f2a8a0f3e0692e0138c9e736
Michelle <mello.s.michelle@gmail.com>
2021-10-16 20:10:20 -0300

    Videos 2, 3 and 4

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,6 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+//utilizar mesmo padrão de variáveis
+//usar const para criação de variáveis
+//camelCase
---
+++ 4-convesaoDeTipos.js
@@ -0,0 +1,8 @@
+console.log("Conversão de Tipos");
+
+console.log("ano " + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2"); //converte por ser operação matemática
+console.log("Texto" / "2"); //Not a number
